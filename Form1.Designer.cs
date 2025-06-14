3fbabe7d66e2bf742486bdc7c63d54288f0ff4b2
Daniel <20250019@ricaldone.edu.sv>
2025-06-14 11:20:06 -0600

    Descuento

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Calcular_el_descuento_de_un_producto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gbPrincipal = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblOriginal = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.txtOriginal = new System.Windows.Forms.TextBox();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.btnDescuento = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.gbPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbPrincipal
+            // 
+            this.gbPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.gbPrincipal.Controls.Add(this.lblError);
+            this.gbPrincipal.Controls.Add(this.btnLimpiar);
+            this.gbPrincipal.Controls.Add(this.btnDescuento);
+            this.gbPrincipal.Controls.Add(this.txtDescuento);
+            this.gbPrincipal.Controls.Add(this.txtOriginal);
+            this.gbPrincipal.Controls.Add(this.lblDescuento);
+            this.gbPrincipal.Controls.Add(this.lblOriginal);
+            this.gbPrincipal.Location = new System.Drawing.Point(119, 40);
+            this.gbPrincipal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbPrincipal.Name = "gbPrincipal";
+            this.gbPrincipal.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbPrincipal.Size = new System.Drawing.Size(598, 241);
+            this.gbPrincipal.TabIndex = 0;
+            this.gbPrincipal.TabStop = false;
+            this.gbPrincipal.Text = "Pantalla Principal";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // lblOriginal
+            // 
+            this.lblOriginal.AutoSize = true;
+            this.lblOriginal.Location = new System.Drawing.Point(39, 53);
+            this.lblOriginal.Name = "lblOriginal";
+            this.lblOriginal.Size = new System.Drawing.Size(107, 19);
+            this.lblOriginal.TabIndex = 0;
+            this.lblOriginal.Text = "Precio Original:";
+            this.lblOriginal.Click += new System.EventHandler(this.lblOriginal_Click);
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Location = new System.Drawing.Point(36, 134);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(110, 19);
+            this.lblDescuento.TabIndex = 1;
+            this.lblDescuento.Text = "Total a pagar es:";
+            this.lblDescuento.Click += new System.EventHandler(this.lblDescuento_Click);
+            // 
+            // txtOriginal
+            // 
+            this.txtOriginal.Location = new System.Drawing.Point(40, 86);
+            this.txtOriginal.Name = "txtOriginal";
+            this.txtOriginal.Size = new System.Drawing.Size(100, 26);
+            this.txtOriginal.TabIndex = 2;
+            this.txtOriginal.TextChanged += new System.EventHandler(this.txtOriginal_TextChanged);
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Location = new System.Drawing.Point(40, 174);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.ReadOnly = true;
+            this.txtDescuento.Size = new System.Drawing.Size(100, 26);
+            this.txtDescuento.TabIndex = 3;
+            // 
+            // btnDescuento
+            // 
+            this.btnDescuento.Location = new System.Drawing.Point(304, 53);
+            this.btnDescuento.Name = "btnDescuento";
+            this.btnDescuento.Size = new System.Drawing.Size(144, 44);
+            this.btnDescuento.TabIndex = 4;
+            this.btnDescuento.Text = "Aplicar Descuento";
+            this.btnDescuento.UseVisualStyleBackColor = true;
+            this.btnDescuento.Click += new System.EventHandler(this.btnDescuento_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(304, 127);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(144, 32);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(341, 177);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(45, 19);
+            this.lblError.TabIndex = 6;
+            this.lblError.Text = "Error";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.gbPrincipal);
+            this.Font = new System.Drawing.Font("Harlow Solid Italic", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Calcular el descuento de un producto";
+            this.gbPrincipal.ResumeLayout(false);
+            this.gbPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbPrincipal;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label lblOriginal;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.TextBox txtOriginal;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnDescuento;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcular_el_descuento_de_un_producto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblError.Text = "";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void lblOriginal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDescuento_Click(object sender, EventArgs e)
+        {
+            double total, precio, descuento;
+            if (double.TryParse(txtOriginal.Text,out precio)) 
+            {
+                if (precio <= 0)
+                {
+                    lblError.Text = "Esto no es posible\nIngrese dato numerico valido";
+                    txtDescuento.Text=string.Empty;
+                }
+                else 
+                {
+                    descuento = precio * 0.65;
+                    total = precio - descuento;
+                    txtDescuento.Text = total.ToString("N2");
+                    lblError.Text=string.Empty;
+
+                    
+                }
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lblError.Text=string.Empty;
+            txtDescuento.Text=string.Empty;
+            txtOriginal.Text=string.Empty;
+        }
+
+        private void txtOriginal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDescuento_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
